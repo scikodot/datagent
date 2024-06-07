5c3e1c01b08590d4aa79d84b24452e09a9b525a3
Dan <daniladenisov729@gmail.com>
2024-06-07 20:23:32 +0300

    Move CustomFileSystemInfoCollection to a separate file

--- DatagentMonitor/Collections/GroupedLookupLinkedList.cs
+++ DatagentMonitor/Collections/GroupedLookupLinkedList.cs
@@ -135,6 +135,15 @@ public abstract class GroupedLookupLinkedList<TKey, TValue> : ICollection<TValue
 
     public void CopyTo(TValue[] array, int arrayIndex) => _list.CopyTo(array, arrayIndex);
 
+    public IEnumerable<TGroup> EnumerateGroup<TGroup>() where TGroup : TValue
+    {
+        if (!_groups.TryGetValue(typeof(TGroup).Name, out var group))
+            yield break;
+
+        foreach (var item in group.Select(i => (TGroup)i))
+            yield return item;
+    }
+
     public IEnumerator<TValue> GetEnumerator() => _list.GetEnumerator();
 
     IEnumerator<TValue> IEnumerable<TValue>.GetEnumerator() => GetEnumerator();
@@ -185,15 +194,6 @@ public abstract class GroupedLookupLinkedList<TKey, TValue> : ICollection<TValue
         group.Remove(node);
         if (group.Count == 0)
             _groups.Remove(type);
-    }
-
-    public IEnumerable<TGroup> EnumerateGroup<TGroup>() where TGroup : TValue
-    {
-        if (!_groups.TryGetValue(typeof(TGroup).Name, out var group))
-            yield break;
-
-        foreach (var item in group.Select(i => (TGroup)i))
-            yield return item;
     }
 
     public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
--- DatagentMonitor/FileSystem/CustomFileSystemInfo.cs
+++ DatagentMonitor/FileSystem/CustomFileSystemInfo.cs
@@ -1,38 +1,4 @@
-﻿using DatagentMonitor.Collections;
-
-namespace DatagentMonitor.FileSystem;
-
-public class CustomFileSystemInfoCollection : GroupedLookupLinkedList<string, CustomFileSystemInfo>
-{
-    public IEnumerable<CustomDirectoryInfo> Directories => EnumerateGroup<CustomDirectoryInfo>();
-    public IEnumerable<CustomFileInfo> Files => EnumerateGroup<CustomFileInfo>();
-
-    protected override string GetKey(CustomFileSystemInfo info) => info.Name;
-
-    public override void Add(CustomFileSystemInfo node)
-    {
-        base.Add(node);
-
-        node.NamePropertyChanged += OnRenamed;
-    }
-
-    protected override void Remove(LinkedListNode<CustomFileSystemInfo> node)
-    {
-        base.Remove(node);
-
-        node.Value.NamePropertyChanged -= OnRenamed;
-    }
-
-    // Rename callback that only moves the element to a new key in the lookup; 
-    // element's positions in both list and group remain the same
-    private void OnRenamed(object? sender, CustomRenameEventArgs e)
-    {
-        if (!_lookup.Remove(e.OldName, out var node))
-            throw new KeyNotFoundException(e.OldName);
-
-        _lookup.Add(e.Name, node);
-    }
-}
+﻿namespace DatagentMonitor.FileSystem;
 
 public delegate void CustomRenameEventHandler(object sender, CustomRenameEventArgs e);
 
